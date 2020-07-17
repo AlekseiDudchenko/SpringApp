f04436745fe69ad7cb1d733b2017a24b917afe37
Aleksei Dudchenko <aleksei.dudchenko@med.uni-heidelberg.de>
2020-07-17 15:00:22 +0200

    dev. dependency injection. inject argument to constructor.

--- src/main/java/com/dudch/App.java
+++ src/main/java/com/dudch/App.java
@@ -4,17 +4,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
 
-        Car myTruck = new Truck();
         Car myPassengerCar = context.getBean("myPassengerCarClass", Car.class);
+        Car myTruck = new Truck(new Driver());
+        Car secondTruck = context.getBean("myTruckClass", Car.class);
 
         myPassengerCar.drive();
         myTruck.drive();
+        secondTruck.drive();
 
         context.close();
-
-        }
+    }
 }
--- src/main/java/com/dudch/Truck.java
+++ src/main/java/com/dudch/Truck.java
@@ -2,8 +2,16 @@ package com.dudch;
 
 public class Truck implements Car {
 
+    Driver driver;
+
+    public Truck(Driver driver) {
+        this.driver = driver;
+    }
+
+
     @Override
     public void drive() {
+        driver.greet();
         System.out.println("Truck drives!");
     }
 }
